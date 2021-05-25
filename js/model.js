b078dd9920156c4875d2c51c68fbaf777e662f0a
alperbayram <alperbayrm@hotmail.com>
2021-05-25 23:16:52 +0300

    v0.1

--- js/model.js
+++ js/model.js
@@ -10,25 +10,19 @@
 */
 
 
-
-
-
 var container;
 var camera, scene, renderer;
 var directionalLight;
 
 var mouseX = 0, mouseY = 0;
 
-// Ekran boyutunu belirliyoruz
-// ekranın ortasına göre.
+
 var windowHalfX = window.innerWidth / 2;
 var windowHalfY = window.innerHeight / 2;
 
-
-//boyutunu küçülttüm modelin şekli bozulduğu için
 var sizeX = 500, sizeY = 500;
 
-//obj değişkenimiz 
+
 var brain;
 
 init();
@@ -36,20 +30,19 @@ animate();
 
 
 function init() {
-    
-    // div in içine ıdsi #brain olanın içine yerleşitiyoruz
+  
     var puthere = document.getElementById("brain");
     container = document.createElement( 'div' );
     puthere.appendChild( container );
 
-    // eğer modeli küçültmeseydim tam ekran olacaktı
+  
     // camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 2000 );
     camera = new THREE.PerspectiveCamera( 45, sizeX / sizeY, 1, 2000 );
     camera.position.z = -10;
 
 
 
-    // sahne
+  
     scene = new THREE.Scene();
 
     var ambient = new THREE.AmbientLight( 0x111111 );
@@ -59,7 +52,7 @@ function init() {
     directionalLight.position.set( 0, 0, -1 );
     scene.add( directionalLight );
 
-    // texture , dokusu
+
 
     var manager = new THREE.LoadingManager();
     manager.onProgress = function ( item, loaded, total ) {
@@ -70,8 +63,7 @@ function init() {
 
 
 
-    // model
-    // beyin modelini yüklüyoruz
+  
     var loader = new THREE.OBJLoader( manager );
     loader.load( 'obj/freesurff.OBJ', function ( object ) {
 
@@ -81,10 +73,10 @@ function init() {
 
     } );
 
-    //WebGL oluşturucu, sahnelerinizi WebGL kullanarak görüntüler.
+
     renderer = new THREE.WebGLRenderer();
     renderer.setSize( 500, 500 );
-    // renderer.setSize( window.innerWidth, window.innerHeight );
+ 
     container.appendChild( renderer.domElement );
     document.addEventListener( 'mousemove', onDocumentMouseMove, false );
 
@@ -120,8 +112,7 @@ function render() {
     var r = 7;
     var s = 0.01;
 
-    // kamera yörüngesi
-    //görütülediğimiz bölgelerin aydınlanması için 
+  
     camera.position.x = r * Math.sin( mouseX * s ) * Math.cos(mouseY/2 * s);
     camera.position.z = -r * Math.cos( mouseX * s ) * Math.cos(mouseY/2 * s);
     camera.position.y = r * Math.sin(mouseY/2 * s);
